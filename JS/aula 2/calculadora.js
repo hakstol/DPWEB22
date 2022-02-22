a407a726725c3c1d2ff2d795bca3b3f3465d8aac
hakstol <lv.hakstol@gmail.com>
2022-02-21 21:57:56 -0300

    finalizado desafio 04 login

--- JS/aula 2/calculadora.js
+++ JS/aula 2/calculadora.js
@@ -1,13 +1,13 @@
 function calcularMedia() {
     var numeroUm = parseFloat(prompt("Digite o primeiro numero:"))
-var numeroDois = parseFloat(prompt("Digite o segundo numero:"))
+    var numeroDois = parseFloat(prompt("Digite o segundo numero:"))
 
-var resultadoMedia = (numeroUm + numeroDois) / 2
+    var resultadoMedia = (numeroUm + numeroDois) / 2
 
-// alert("O resultado da média entre os números " + numeroUm + " e " + numeroDois + " é " + resultadoMedia)
-alert(`O resultado da média entre os números ${numeroUm} e ${numeroDois} é ${resultadoMedia}`) 
-console.log("o resultado da media é:" + resultadoMedia)
+    // alert("O resultado da média entre os números " + numeroUm + " e " + numeroDois + " é " + resultadoMedia)
+    alert(`O resultado da média entre os números ${numeroUm} e ${numeroDois} é ${resultadoMedia}`)
+    console.log("o resultado da media é:" + resultadoMedia)
 
-console.log("o tipo da variavel é 1:" + typeof(numeroUm));
-console.log("o tipo da variavel é 1:" + typeof(numeroDois));
+    console.log("o tipo da variavel é 1:" + typeof (numeroUm));
+    console.log("o tipo da variavel é 1:" + typeof (numeroDois));
 }
