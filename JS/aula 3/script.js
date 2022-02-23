b19d3089ac463a876846aed712b4363bbee31966
FIC <fic@info.scs>
2022-02-23 10:14:32 -0300

    finalizado js, iniciar css para calculadora

--- JS/aula 3/script.js
+++ JS/aula 3/script.js
@@ -1,8 +1,38 @@
-function somarNumeros() {
-    var num1 = document.getElementById("input_numero1").value;
-    var num2 = document.getElementById("input_numero2").value;
+//O elemento input_numero1
+const inputNum1 = document.getElementById("input_numero1");
 
-    var resultadoSoma = num1 + num2; // resultado da soma entre input 1 e 2
-    
-    document.getElementById("input_resultado").value = resultadoSoma;
+//O elemento input_numero2
+const inputNum2 = document.getElementById("input_numero2");
+
+//O elemento input_resultado
+const inputResultado = document.getElementById("input_resultado");
+
+function somarNumeros() {
+    // resultado da soma entre input 1 e 2
+    let resultadoSoma = Number(inputNum1.value) + Number(inputNum2.value); // pega o valor da variavel
+    inputResultado.value = resultadoSoma; // retorna o valor dentro do input
+}
+
+function subtrairNumeros() {
+    // resultado da subtracao entre o input 1 e 2
+    let resultadoSubtracao = Number(inputNum1.value) - Number(inputNum2.value);
+    inputResultado.value = resultadoSubtracao;
+}
+
+function multiplicarNumeros() {
+    // resultado da subtracao entre o input 1 e 2
+    let resultadoMultiplicacao = Number(inputNum1.value) * Number(inputNum2.value);
+    inputResultado.value = resultadoMultiplicacao;
+}
+
+function dividirNumeros() {
+    // resultado da subtracao entre o input 1 e 2
+    let resultadoDivisao = Number(inputNum1.value) / Number(inputNum2.value);
+    inputResultado.value = resultadoDivisao;
+}
+
+function limparInputs() {
+    inputNum1.value = '';
+    inputNum2.value = '';
+    inputResultado.value = '';
 }
